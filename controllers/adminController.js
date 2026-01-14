580d2646197ad13efa562753160633298ee698b8
ashok770 <ashoktamata6002@gmail.com>
2026-01-14 09:57:45 +0000

    added the database mongodba and basic backend code

---
+++ controllers/adminController.js
@@ -0,0 +1,31 @@
+import Admin from "../models/Admin.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Admin Login
+export const adminLogin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const admin = await Admin.findOne({ username });
+    if (!admin) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import bcrypt from "bcryptjs";
+import Admin from "./models/Admin.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/admin", adminRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Mobile Shop API running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const createAdmin = async () => {
+  const exists = await Admin.findOne({ username: "admin" });
+  if (!exists) {
+    const hashedPassword = await bcrypt.hash("admin123", 10);
+    await Admin.create({
+      username: "admin",
+      password: hashedPassword,
+    });
+    console.log("Admin created");
+  }
+};
+
+createAdmin();
