ae3163c0d968b2de01fe931c2f32faa19c8bc197
ashok770 <ashoktamata6002@gmail.com>
2026-01-18 17:31:55 +0000

    woworking on the styling part of the admin panel

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,6 +5,7 @@ import {
   updateOrderStatus,
 } from "../controllers/orderController.js";
 import protect from "../middleware/authMiddleware.js";
+import Order from "../models/Order.js";
 
 const router = express.Router();
 
@@ -14,5 +15,37 @@ router.post("/", createOrder);
 // Admin only
 router.get("/", protect, getOrders);
 router.put("/:id", protect, updateOrderStatus);
+
+// GET admin stats
+router.get("/stats/admin", protect, async (req, res) => {
+  const orders = await Order.find();
+
+  const totalOrders = orders.length;
+  const pendingOrders = orders.filter(
+    (o) => o.orderStatus === "Pending"
+  ).length;
+
+  const deliveredOrders = orders.filter(
+    (o) => o.orderStatus === "Delivered"
+  );
+
+  const totalRevenue = deliveredOrders.reduce((sum, order) => {
+    return (
+      sum +
+      order.items.reduce(
+        (itemSum, item) =>
+          itemSum + item.price * item.quantity,
+        0
+      )
+    );
+  }, 0);
+
+  res.json({
+    totalOrders,
+    pendingOrders,
+    deliveredOrders: deliveredOrders.length,
+    totalRevenue,
+  });
+});
 
 export default router;
