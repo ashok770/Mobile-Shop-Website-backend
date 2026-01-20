3e296d4e8ad35a1e80165c8c5e2e94ef51dfbe83
ashok770 <ashoktamata6002@gmail.com>
2026-01-20 12:31:21 +0000

    added the offers

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,11 +8,23 @@ import {
 
 import protect from "../middleware/authMiddleware.js";
 import upload from "../middleware/upload.js";
+import Product from "../models/Product.js";
 
 const router = express.Router();
 
 // Public
 router.get("/", getProducts);
+
+// GET products by offer
+router.get("/offers/:type", async (req, res) => {
+  const { type } = req.params;
+
+  const products = await Product.find({
+    offerType: type
+  });
+
+  res.json(products);
+});
 
 // Admin
 router.post("/", protect, upload.single("image"), createProduct);
